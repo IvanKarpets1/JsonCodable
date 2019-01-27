743c79788194c1c8ac84445b15357097e827385c
IvanKarpets <ivankarpets@icloud.com>
2019-01-27 18:55:53 +0200

    Initial Commit

---
+++ jsonCodable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonCodable
+//
+//  Created by Ivan Karpets on 1/27/19.
+//  Copyright © 2019 Ivan Karpets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

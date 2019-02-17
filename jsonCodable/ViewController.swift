5fda1b1b46636d84d8ab71ff2a3c04ad3112ffe0
IvanKarpets <ivankarpets@icloud.com>
2019-02-17 19:30:54 +0200

    first_and_last

--- jsonCodable/ViewController.swift
+++ jsonCodable/ViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
+struct WebSiteDescription: Decodable {
+    let name: String
+    let description: String
+    let courses: [Course]
+}
+struct Course: Decodable{
+    let id: Int?
+    let name: String?
+    let link: String?
+    let imageUrl: String?
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        let urlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+        
+            guard let dataString = data else { return }
+            
+            do{
+              let courses = try JSONDecoder().decode([Course].self, from: dataString)
+                print(courses)
+                
+            }catch let serializationErr{
+                print(serializationErr)
+            }
+            
+        }.resume()
     }
 
 
